15f7e0449775ef2a6eda87a57fd85fa640a74958
Levi Kohler <kohler.la01@gmail.com>
2021-05-07 14:44:24 -0700

    add map marker info window

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  InfoWindow,
   withScriptjs,
   withGoogleMap,
   GoogleMap,
@@ -7,6 +8,23 @@ import {
 } from "react-google-maps";
 
 class App extends React.Component {
+
+  state = {
+    address: '',
+    city: '',
+    area: '',
+    state: '',
+    zoom: 15,
+    height: 400,
+    mapPosition: {
+      lat: 0,
+      lng: 0,
+    },
+    markerPosition: {
+      lat: 0,
+      lng: 0,
+    }
+  }
   
   render() {
     const MapWithAMarker = withScriptjs(withGoogleMap(props =>
@@ -16,13 +34,19 @@ class App extends React.Component {
       >
         <Marker
           position={{ lat: -34.397, lng: 150.644 }}
-        />
+        >
+          <InfoWindow>
+            <div>
+              hello
+            </div>
+          </InfoWindow>
+        </Marker>
       </GoogleMap>
     ));
   
     return ( 
       <MapWithAMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=API_KEY_GOES_HERE&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=API_KEY&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
