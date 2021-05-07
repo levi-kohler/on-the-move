ab3817e2be8d010f77c588400892ca831f51b45e
Levi Kohler <kohler.la01@gmail.com>
2021-05-07 12:06:10 -0700

    have been trying to hide api key in dotenv file but havent got application to load without having key directly in file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   Marker,
 } from "react-google-maps";
 
-
 class App extends React.Component {
   
   render() {
@@ -23,7 +22,7 @@ class App extends React.Component {
   
     return ( 
       <MapWithAMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key={YOUR_API_KEY_GOES_HERE}&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=API_KEY_GOES_HERE&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
