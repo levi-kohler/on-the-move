b0aca81e163ce18e4a4786d9fb297fec05033441
Levi Kohler <kohler.la01@gmail.com>
2021-05-07 11:04:40 -0700

    update App.js to utilize Google Maps API, successfully loads Google Maps onto DOM when inputting the API key, remove key for this commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-function App(){
-  return ( 
-    <React.Fragment>
-      <h1>Hello  world!</h1>
-    </React.Fragment>
-  );
+
+class App extends React.Component {
+  
+  render() {
+    const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      >
+        <Marker
+          position={{ lat: -34.397, lng: 150.644 }}
+        />
+      </GoogleMap>
+    ));
+  
+    return ( 
+      <MapWithAMarker
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key={YOUR_API_KEY_GOES_HERE}&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
 }
 
 export default App;
