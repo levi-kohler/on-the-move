764c6e9ba54de4b793a22922c8ced4d6381e6694
Levi Kohler <kohler.la01@gmail.com>
2021-05-07 16:12:41 -0700

    write functions in App.js to find city, area, and state info from latitude and longitude from Google Maps API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import GeoCode from "react-geocode";
+
+GeoCode.setApiKey("API_KEY")
 
 class App extends React.Component {
 
@@ -25,6 +28,60 @@ class App extends React.Component {
       lng: 0,
     }
   }
+
+  getCity = (addressArray) => {
+    let city = '';
+    for(let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0] && 'administrative_area_level_2' === addressArray[i].types[0]) {
+        city = addressArray[i].long_name;
+        return city;
+      }
+    }
+  }
+
+  getArea = (addressArray) => {
+    let area = '';
+    for(let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0]) {
+        for(let j = 0; j< addressArray.length; j++) {
+          if('sublocality_level_1' === addressArray[i].types[j] || 'locality' === addressArray[i].types[j]) {
+            area = addressArray[i].long_name;
+            return area;
+          }
+        }
+      }
+    }
+  }
+
+  getState = (addressArray) => {
+    let state = '';
+    for(let i = 0; i < addressArray.length; i++) {
+      for(let i = 0; i < addressArray.length; i++) {
+        if(addressArray[i].types[0] && 'administrative_area_1' === addressArray[i].types[0]) {
+          state = addressArray[i].long_name;
+          return state;
+        }
+      }
+    }
+  }
+
+
+  onMarkerDragEnd = (event) => {
+
+    let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
+
+    GeoCode.fromLatLng(newLat, newLng)
+    .then(response => {
+
+      const address = response.results[0].formatted_address,
+          addressArray = response.result[0].address_components,
+          city = this.getCity(addressArray),
+          area = this.getArea(addressArray),
+          state = this.getSnapshotBeforeUpdate(addressArray)
+
+    })
+  }
   
   render() {
     const MapWithAMarker = withScriptjs(withGoogleMap(props =>
@@ -33,6 +90,8 @@ class App extends React.Component {
         defaultCenter={{ lat: -34.397, lng: 150.644 }}
       >
         <Marker
+        draggable={true}
+        onDragEnd={this.onMarkerDragEnd}
           position={{ lat: -34.397, lng: 150.644 }}
         >
           <InfoWindow>
